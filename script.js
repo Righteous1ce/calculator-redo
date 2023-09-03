596ca5c894b932119f9324b13c2cfda1d719e3eb
Righteous1ice <hoplabradley@gmail.com>
2023-09-03 20:03:07 +0100

    Changed the method of selection

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButton = document.querySelectorAll('[data-operator]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearButton = document.querySelectorAll('[data-all-clear]');
+const equalsButton = document.querySelectorAll('[data-equals]')
+const upperDisplay = document.querySelector('[data-upper-display]');
+const lowerDisplay = document.querySelector('[data-lower-display]');
