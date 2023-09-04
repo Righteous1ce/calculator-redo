fc13721eff938f98e709fb48231b3f588961d14c
Righteous1ice <hoplabradley@gmail.com>
2023-09-04 17:08:10 +0100

    console logging number btns and operator btns

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButton = document.querySelectorAll('[data-operator]');
 const deleteButton = document.querySelectorAll('[data-delete]');
@@ -5,3 +6,35 @@ const allClearButton = document.querySelectorAll('[data-all-clear]');
 const equalsButton = document.querySelectorAll('[data-equals]')
 const upperDisplay = document.querySelector('[data-upper-display]');
 const lowerDisplay = document.querySelector('[data-lower-display]');
+
+
+
+
+//console.log(operate(firstOperand, operator, secondOperand));
+
+
+
+numberButtons.forEach((button => {
+    button.addEventListener('click', () =>{
+        console.log(button);
+    })
+}))
+
+operatorButton.forEach((button => {
+    button.addEventListener('click', () =>{
+        console.log(button);
+    })
+}))
+
+function add(firstOperand, secondOperand){
+    return firstOperand + secondOperand;
+}
+function subtarct(firstOperand, secondOperand){
+    return firstOperand - secondOperand;
+}
+function multiply(firstOperand, secondOperand){
+    return firstOperand * secondOperand;
+}
+function divide(firstOperand, secondOperand){
+    return firstOperand / secondOperand;
+}
